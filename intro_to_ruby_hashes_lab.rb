97ce78548ae37d8023b174df2d6f52542f5342cf
Jordan Sanchez <jrdnsanchez@gmail.com>
2019-09-11 20:55:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,25 +36,32 @@ end
 
 def monopoly_with_fourth_tier
   monopoly = {
-	  railroads: {
+	  :railroads => 
+	  {
 	    pieces: 4,
-      rent_in_dollars: {
+      rent_in_dollars: =>
+      {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
         :four_pieces_owned => 200
       },
-      names: {
-        :reading_railroad => {
+      :names =>
+      {
+        :reading_railroad => 
+        {
           "mortagage_value" => "100"
         },
-        :pennsylvania_railroad => {
+        :pennsylvania_railroad => 
+        {
           "mortagage_value" => "200"
         },
-        :b_and_o_railroad => {
+        :b_and_o_railroad => 
+        {
           "mortagage_value" => "400"
         },
-        :shortline => {
+        :shortline => 
+        {
           "mortagage_value" => "800"
         }
       }
